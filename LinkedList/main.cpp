62036fb5278fd4edb5cd4b7978b9543fc4f675c2
Neil-Moran <neilmoran007@gmail.com>
2022-04-04 17:33:48 +0100

    Add function to add values from input file

--- LinkedList/main.cpp
+++ LinkedList/main.cpp
@@ -1,13 +1,34 @@
+#include <stdio.h>
 #include "LinkedList.h"
+
+void addFromFile(LinkedList &list, char *filename);
 
 int main() 
 {
     LinkedList list;
-    list += new Node(1);
-    list += new Node(5);
-    list += new Node(72);
+
+    addFromFile(list, "input.txt");
 
     list.print();
 
     return 0;
+}
+
+void addFromFile(LinkedList &list, char *filename)
+{
+    FILE *file;
+    file = fopen(filename, "r");
+    
+    // The first value in the file indicates the number of values
+    int numValues;
+    fscanf_s(file, "%d", &numValues);
+    int v = -99999;
+
+    for(int i=0; i<numValues; ++i)
+    {
+        fscanf_s(file, "%d", &v);
+        list += new Node(v);
+    }
+
+    return;
 }
