93d14909dffc88ed6ae21c75ea794fc993453f37
Neil-Moran <neilmoran007@gmail.com>
2022-04-05 13:15:23 +0100

    Add function to insert numbers from file

--- LinkedList/main.cpp
+++ LinkedList/main.cpp
@@ -2,6 +2,7 @@
 #include "LinkedList.h"
 
 void addFromFile(LinkedList &list, char *filename);
+void insertFromFile(LinkedList &list, char *filename);
 
 int main() 
 {
@@ -15,12 +16,8 @@ int main()
 
     {
         LinkedList listOrdered;
-        listOrdered.insert(new Node(4));
-        listOrdered.insert(new Node(1));
-        listOrdered.insert(new Node(3));
-        listOrdered.insert(new Node(5));
-        listOrdered.insert(new Node(2));
-        listOrdered.print(); // list of 1-5 ordered
+        insertFromFile(listOrdered, "scrambledNumbers.txt");
+        listOrdered.print(); // list of 1-20 ordered
     }
 
     return 0;
@@ -40,6 +37,25 @@ void addFromFile(LinkedList &list, char *filename)
     {
         fscanf_s(file, "%d", &v);
         list += new Node(v);
+    }
+
+    return;
+}
+
+void insertFromFile(LinkedList &list, char *filename)
+{
+    FILE *file;
+    file = fopen(filename, "r");
+    
+    // The first value in the file indicates the number of values
+    int numValues;
+    fscanf_s(file, "%d", &numValues);
+    int v = -99999;
+
+    for(int i=0; i<numValues; ++i)
+    {
+        fscanf_s(file, "%d", &v);
+        list.insert(new Node(v));
     }
 
     return;
