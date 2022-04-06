eec634b3f36585fe70341f303b3612adf3f6999d
Neil-Moran <neilmoran007@gmail.com>
2022-04-06 12:29:01 +0100

    Replace fopen and fscan with fopen_s and fscan_s

--- CelsiusToFahrenheit/main.cpp
+++ CelsiusToFahrenheit/main.cpp
@@ -8,21 +8,20 @@ int main()
     using std::vector;
 
     FILE *inputFile;
-    inputFile = fopen("input.txt", "r");
+    fopen_s(&inputFile, "input.txt", "r");
 
     // The first value in the file indicates the number of values
     int numValues;
-    fscanf(inputFile, "%d", &numValues);
+    fscanf_s(inputFile, "%d", &numValues);
     vector<float> celValues(numValues);
 
     // Read and convert the temperature values
     for (int i=0; i<numValues; i++)
     {
-        fscanf(inputFile, "%f", &celValues[i]);
-        float t_fahr = (celValues[i]*1.8) + 32;
+        fscanf_s(inputFile, "%f", &celValues[i]);
+        float t_fahr = (celValues[i]*1.8f) + 32.0f;
         printf("%.2f Celsius = %.2f Fahrenheit!\n", celValues[i], t_fahr);
     }
 
     return 0;
-
 }
