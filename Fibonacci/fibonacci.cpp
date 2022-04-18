0ebc6a8a0957a889f04e18fa918f5186daae8c18
Neil-Moran <neilmoran007@gmail.com>
2022-04-18 17:55:29 +0100

    Add methods to print nth term of series

--- Fibonacci/fibonacci.cpp
+++ Fibonacci/fibonacci.cpp
@@ -26,6 +26,39 @@ void printFibonacci(int n)
     }
 
     printf("\n");
+}
 
-    return;
+unsigned long long calcFibonacciTermRecursive(int n) //saw this algorithm online and it looked fun, but it's extremely slow!
+{
+    if(n<2) return unsigned long long(n); //0th and 1st terms are 0 and 1
+    
+    return calcFibonacciTermRecursive(n-2) + calcFibonacciTermRecursive(n-1); //else return sum of two previous terms
+}
+
+void printFibonacciTermRecursive(int n)
+{
+    if(n>76) printf("Cannot print Fibonacci terms above #76, they're too big. Sorry!\n");
+    else printf("Printing Fibonacci term #%d recursively: %I64d\n", n, calcFibonacciTermRecursive(n));
+}
+
+unsigned long long calcFibonacciTerm(int n)
+{
+    if(n<2) return unsigned long long(n); //0th and 1st terms are 0 and 1
+    
+    unsigned long long prev = 0;
+    unsigned long long curr = 1; 
+
+    for(int i=1; i<n; ++i) //iterate until the nth term
+    {   
+        unsigned long long next = prev + curr;
+        prev = curr;
+        curr = next;
+    }
+    return curr; 
+}
+
+void printFibonacciTerm(int n)
+{
+    if(n>76) printf("Cannot print Fibonacci terms above #76, they're too big. Sorry!\n");
+    else printf("Printing Fibonacci term #%d: %I64d\n", n, calcFibonacciTerm(n));
 }
--- Fibonacci/main.cpp
+++ Fibonacci/main.cpp
@@ -1,4 +1,5 @@
 #include "fibonacci.cpp"
+#include <stdio.h>
 
 int main()
 {
@@ -6,6 +7,22 @@ int main()
     printFibonacci(1);
     printFibonacci(10);
     printFibonacci(20);
+    printf("\n");
+
+    printFibonacciTermRecursive(0);
+    printFibonacciTermRecursive(1);
+    printFibonacciTermRecursive(30);
+    printFibonacciTermRecursive(50);
+    printFibonacciTermRecursive(77);
+    printf("\n");
+
+    printFibonacciTerm(0);
+    printFibonacciTerm(1);
+    printFibonacciTerm(30);
+    printFibonacciTerm(50);
+    printFibonacciTerm(76);
+    printFibonacciTerm(77);
+    printf("\n");
 
     return 0;
 }
