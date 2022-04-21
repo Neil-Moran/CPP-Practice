f331f2278ac70a24b4f3ad0171ee0296631957b4
Neil-Moran <neilmoran007@gmail.com>
2022-04-21 16:54:54 +0100

    Update loop to skip even numbers

--- Primes/primes.cpp
+++ Primes/primes.cpp
@@ -4,11 +4,12 @@
 void primes(int n) //print the first n primes
 {
     if(n==0) return;
-    printf("First %d primes: ", n);
+    printf("First %d primes: 2 ", n); //print 2 as we know it's the first prime
+    --n; //decrement prime counter
 
-    unsigned long long primeList = ULL_MAX; //list of 1s denoting which indices are prime. We will unset non-prime indices below using the sieve of eratosthenes
+    unsigned long long primeList = ULL_MAX; //binary list of 1s denoting which indices are prime. We will unset non-prime indices below using the sieve of eratosthenes
 
-    int i=2; //skip 0 and 1, we know they're not prime
+    int i=3; //start on 3, the next prime
 
     while(n>0) //loop until we've printed n primes
     {
@@ -16,13 +17,15 @@ void primes(int n) //print the first n primes
         {
             printf("%d ", i);
 
-            for(int j=i+i; j<sizeof(unsigned long long)*8; j+=i) //unset prime flag for every factor of i
+            for(int j=3*i; j<sizeof(unsigned long long)*8; j+=i) //unset prime flag for every factor of i
+            //we start at j = 3i as we've just processed i and 2i must be even, 
+            //we already skip even integers by incrementing i by 2 below
             {
                 primeList &= ~(1ULL << j);
             }
             --n; //decrement prime counter
         }
-        ++i; //check if next number is prime
+        i+=2; //check if next odd number is prime
     }
 
     printf("\n");
