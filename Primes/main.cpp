4d601468f7e0921066de09f713e9f6b42f410150
Neil-Moran <neilmoran007@gmail.com>
2022-04-28 22:11:37 +0100

    Fix typo

--- Primes/main.cpp
+++ Primes/main.cpp
@@ -3,6 +3,6 @@
 void main()
 {
     primes(1000);
-    printf("Is 1000 prime: %s\n", isPrime(999) ? "True" : "False");
+    printf("Is 1000 prime: %s\n", isPrime(1000) ? "True" : "False");
     printf("Is 9999991 prime: %s\n", isPrime(9999991) ? "True" : "False");
 }
