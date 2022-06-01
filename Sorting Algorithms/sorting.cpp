a23926986a25b7fa154fa59200429ec6b8d49fb9
Neil-Moran <neilmoran007@gmail.com>
2022-06-01 16:16:54 +0100

    Add merge sort

--- Sorting Algorithms/sorting.cpp
+++ Sorting Algorithms/sorting.cpp
@@ -1,5 +1,7 @@
+#pragma once
 #include "sorting.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 void print(int array[], int size) 
 {
@@ -32,6 +34,89 @@ void bubbleSort(int array[], int size)
             }
         }
     }
+
+    print(array, size);
+}
+
+void merge(int array[], int l, int mid, int r) //assuming array is sorted from l->mid & mid+1->r, merge those two sub arrays
+{
+     
+    int *arrayTemp = (int*)malloc(sizeof(int)*(r-l+1));
+
+    int i=0;
+    int i_l = l;
+    int i_r = mid+1;  
+    
+    while(i<=r-l && i_l <= mid && i_r <= r)
+    {
+        if(array[i_l] < array[i_r])
+        {
+            arrayTemp[i] = array[i_l];
+            ++i_l;
+        }
+        else 
+        {
+            arrayTemp[i] = array[i_r];
+            ++i_r;
+        }
+
+        ++i;
+    }
+
+    if(i_l <= mid)
+    {
+        for(int j=i_l; j<=mid; ++j)
+        {
+            arrayTemp[i] = array[j];
+            ++i;
+        }
+    }
+
+    if(i_r <= r)
+    {
+        for(int j=i_r; j<=r; ++j)
+        {
+            arrayTemp[i] = array[j];
+            ++i;
+        }
+    }
+    
+    for(i=0; i<=r-l; ++i)
+        array[l+i] = arrayTemp[i];
+
+    free(arrayTemp);
+}
+
+void mergeSort(int array[], int l, int r)
+{
+    if(l == r) return;    
+
+    if(l == r-1)
+    {
+        if(array[l] > array[r])
+        {
+            int temp = array[l];
+            array[l] = array[r];
+            array[r] = temp;
+        }
+        return;
+    }
+
+    int mid = l + ( (r-l)/2 );
+
+    mergeSort(array, l, mid);
+    mergeSort(array, mid+1, r);
+
+    merge(array, l, mid, r);
+}
+
+void mergeSort(int array[], int size)
+{    
+    if(size <= 0) return;
+
+    printf("Merge Sort: ");
+    
+    mergeSort(array, 0, size-1);
 
     print(array, size);
 }
--- Sorting Algorithms/sorting.h
+++ Sorting Algorithms/sorting.h
@@ -1,3 +1,4 @@
 void print(int array[], int size);
 
 void bubbleSort(int array[], int size);
+void mergeSort(int array[], int size);
