e01fc1623109101db563013f4e8a246180711e0a
Neil-Moran <neilmoran007@gmail.com>
2022-06-03 15:32:33 +0100

    Add selection sort

--- Sorting Algorithms/main.cpp
+++ Sorting Algorithms/main.cpp
@@ -1,10 +1,12 @@
 #include "sorting.h"
+#include <stdio.h>
 
 int main()
 {
     {
     int array[] = {16, 6, 8, 11, 10, 20, 25, 23, 26, 30, 17, 4, 28, 9, 18, 1, 27, 7, 19, 2, 15, 21, 12, 22, 24, 29, 3, 14, 5, 13};
 
+    printf("Unsorted Array: ");
     print(array, 30);
     bubbleSort(array, 30);
     }
@@ -19,9 +21,17 @@ int main()
     quickSort(array, 30);
     }
 
+    {
+    int array[] = {16, 6, 8, 11, 10, 20, 25, 23, 26, 30, 17, 4, 28, 9, 18, 1, 27, 7, 19, 2, 15, 21, 12, 22, 24, 29, 3, 14, 5, 13};
+    selectionSort(array, 30);
+    }
+
+    printf("\n");
+
     profileBubbleSort();
     profileMergeSort();
     profileQuickSort();
+    profileSelectionSort();
 
     return 0;
 }
