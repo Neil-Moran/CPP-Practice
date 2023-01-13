bede0ab656e8e1464dfc56d540f550ffb8c45d05
Neil-Moran <neilmoran007@gmail.com>
2023-01-13 13:58:26 +0000

    Continue 2d grid/solver refactor

--- Sudoku Solver/sudoku.cpp
+++ Sudoku Solver/sudoku.cpp
@@ -726,7 +726,7 @@ void solver::solveValueForRegion(int value, cell region[]) // checks where speci
         case 2: // pointing pairs: https://sudoku.com/sudoku-rules/pointing-pairs/
         {
             // if current region is a box, check if candidates are also in the same row or column
-            if(isBox(region)) //TODO: test this
+            if(isBox(region)) 
             {
                 if(candidateCells[0].row == candidateCells[1].row) // candidates are in the same row
                 {
@@ -764,11 +764,8 @@ void solver::solveValueForRegion(int value, cell region[]) // checks where speci
                 int rowOffset = 3*(candidateCells[0].row/3);
                 int colOffset = 3*(candidateCells[0].column/3);
 
-                if( (isRow(region)
-                    && (colOffset == 3*(candidateCells[1].column/3)) )
-                    ||
-                    (isColumn(region)
-                    && (rowOffset == 3*(candidateCells[1].row/3)) ))
+                if( (isRow(region) && (colOffset == 3*(candidateCells[1].column/3)))
+                    || (isColumn(region) && (rowOffset == 3*(candidateCells[1].row/3))))
                     {
                         for(int rowIt=rowOffset; rowIt<3+rowOffset; ++rowIt)
                         {
@@ -1009,7 +1006,7 @@ void solver::solveNextNumber()
                region[currRow] = cell(currRow, currCol);
             }
 
-            for(int value=1; value<=9; ++value) // see if we can solve any of the numbers 1-9 for this row
+            for(int value=1; value<=9; ++value) // see if we can solve any of the numbers 1-9 for this column
             {
                 solveValueForRegion(value, region);
 
@@ -1018,6 +1015,31 @@ void solver::solveNextNumber()
 
             // look for hidden pairs
         } // column loop
+
+        cell box[9][9] = 
+        {
+            {cell(0,0), cell(0,1), cell(0,2), cell(1,0), cell(1,1), cell(1,2), cell(2,0), cell(2,1), cell(2,2)},
+            {cell(0,3), cell(0,4), cell(0,5), cell(1,3), cell(1,4), cell(1,5), cell(2,3), cell(2,4), cell(2,5)},
+            {cell(0,6), cell(0,7), cell(0,8), cell(1,6), cell(1,7), cell(1,8), cell(2,6), cell(2,7), cell(2,8)},
+            {cell(3,0), cell(3,1), cell(3,2), cell(4,0), cell(4,1), cell(4,2), cell(5,0), cell(5,1), cell(5,2)},
+            {cell(3,3), cell(3,4), cell(3,5), cell(4,3), cell(4,4), cell(4,5), cell(5,3), cell(5,4), cell(5,5)},
+            {cell(3,6), cell(3,7), cell(3,8), cell(4,6), cell(4,7), cell(4,8), cell(5,6), cell(5,7), cell(5,8)},
+            {cell(6,0), cell(6,1), cell(6,2), cell(7,0), cell(7,1), cell(7,2), cell(8,0), cell(8,1), cell(8,2)},
+            {cell(6,3), cell(6,4), cell(6,5), cell(7,3), cell(7,4), cell(7,5), cell(8,3), cell(8,4), cell(8,5)},
+            {cell(6,6), cell(6,7), cell(6,8), cell(7,6), cell(7,7), cell(7,8), cell(8,6), cell(8,7), cell(8,8)}
+        };
+
+        for(int currBox=0; currBox<9; ++currBox) // boxes
+        {
+            for(int value=1; value<=9; ++value) // see if we can solve any of the numbers 1-9 for this column
+            {
+                solveValueForRegion(value, box[currBox]);
+
+                if(finished) return; // stop if we've solved enough cells, continue otherwise
+            } // value loop
+
+            // look for hidden pairs
+        } // box loop
     }
 }
 
