5a073a9bca3685804966d5ba8bbd2225458c8605
Neil-Moran <neilmoran007@gmail.com>
2023-04-04 11:56:55 +0100

    Update string compare function

--- DoubleLinkedList/stringUtilities.cpp
+++ DoubleLinkedList/stringUtilities.cpp
@@ -3,14 +3,12 @@
 
 bool areStringsAlphabetized(char *s1, char *s2)
 {
-    for(int i=0; s1[i], s2[i]; ++i)
+    for(int i=0; ; ++i)
     {
+        if(!s1[i]) return true;
+        if(!s2[i]) return false;
+
         if(s1[i] < s2[i]) return true;
-        if(s1[i] > s2[i]) return false;
-
-        if(!s1[i+1]) return true;
-        if(!s2[i+1]) return false;
+        if(s1[i] > s2[i]) return false;      
     }
-
-    return false;
 }
