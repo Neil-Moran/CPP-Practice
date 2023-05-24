7b880dd894eb633376178449754370938b820368
Neil-Moran <neilmoran007@gmail.com>
2023-05-24 21:35:33 +0100

    Support signed numbers for convertStringToInt

--- StringUtilities/stringUtilities.cpp
+++ StringUtilities/stringUtilities.cpp
@@ -39,18 +39,20 @@ int convertStringToInt(char * s)
 
     if(size > 11) return 0; // ERROR: string is longer than "+2147483648", will overflow 
 
-    int multiplier = 1;
     int result = 0;
+    int multiplier = (s[0] == '-') ? -1 : 1; // if first char is '-' we want to subtract the running total, else add
+    int firstChar = ((s[0] == '+') || (s[0] == '-')) ? 1 : 0; // we're about to read through all the chars; if first char is +/-, ignore it
 
-    for(int i=size-1; i>=0; --i, multiplier *= 10)
+    for(int i=size-1; i>=firstChar; --i, multiplier *= 10) // read through all numeric chars and add them to result
     {
-        if((multiplier%10 != 0 && multiplier != 1)  // ERROR: overflow, string exceeded max int
+        if((multiplier%10 != 0 && i!=size-1)  // ERROR: overflow, string exceeded max int
             ||(s[i] < '0' || s[i] > '9')) return 0; // ERROR: char is not a digit from 0-9
 
         else result += (s[i]-48) * multiplier;
     }
 
-    if(result < 0) return 0; // ERROR: overflow, string exceeded max int
+    if(result < 0 && s[0] != '-') return 0; // ERROR: overflow, string exceeded max int
+    if(result > 0 && s[0] == '-') return 0; // ERROR: underflow, string exceeded min int
 
     return result;
 }
--- StringUtilities/test_convertStringToInt.h
+++ StringUtilities/test_convertStringToInt.h
@@ -2,54 +2,101 @@
 #include <assert.h>
 #include "stringUtilities.h"
 
-bool test_STOI1_EmptyString()
+bool test_StoI1_EmptyString()
 {
     return (convertStringToInt("") == 0);
 }
 
-bool test_STOI2_0()
+bool test_StoI2_0()
 {
     return (convertStringToInt("0") == 0);
 }
 
-bool test_STOI3_123()
+bool test_StoI3_123()
 {
     return (convertStringToInt("123") == 123);
 }
 
-bool test_STOI4_MaxInt()
+bool test_StoI4_MaxInt()
 {
     return (convertStringToInt("2147483647") == 2147483647);
 }
 
-bool test_STOI5_MaxIntPlus1()
+bool test_StoI5_MaxIntPlus1()
 {
     return (convertStringToInt("2147483648") == 0);
 }
 
-bool test_STOI6_ExtremelyLargeNumber()
+bool test_StoI6_ExtremelyLargeNumber()
 {
     return (convertStringToInt("9999999999999999") == 0);
 }
 
-bool test_STOI7_VeryLargeNumber()
+bool test_StoI7_VeryLargeNumber()
 {
     return (convertStringToInt("92147483647") == 0);
 }
 
-bool test_STOI8_NonNumeric()
+bool test_StoI8_NonNumeric()
 {
     return (convertStringToInt("one") == 0);
 }
 
+bool test_StoI9_NegativeNumber()
+{
+    return (convertStringToInt("-123") == -123);
+}
+
+bool test_StoI10_NegativeMaxInt()
+{
+    return (convertStringToInt("-2147483647") == -2147483647);
+}
+
+bool test_StoI11_PositiveMaxInt()
+{
+    return (convertStringToInt("+2147483647") == +2147483647);
+}
+
+bool test_StoI12_NegativeMaxIntMinus1()
+{
+    return (convertStringToInt("-2147483648") == 0 - _CRT_INT_MAX - 1);
+}
+
+bool test_StoI13_NegativeMaxIntMinus2()
+{
+    return (convertStringToInt("-2147483649") == 0);
+}
+
+bool test_StoI14_NegativeZero()
+{
+    return (convertStringToInt("-0") == 0);
+}
+
+bool test_StoI15_PositiveZero()
+{
+    return (convertStringToInt("+0") == 0);
+}
+
+bool test_StoI16_VeryLargeNegativeNumber()
+{
+    return (convertStringToInt("-92147483647") == 0);
+}
+
 void test_convertStringToInt()
 {
-    assert(test_STOI1_EmptyString());
-    assert(test_STOI2_0());
-    assert(test_STOI3_123());
-    assert(test_STOI4_MaxInt());
-    assert(test_STOI5_MaxIntPlus1());
-    assert(test_STOI6_ExtremelyLargeNumber());
-    assert(test_STOI7_VeryLargeNumber());
-    assert(test_STOI8_NonNumeric());
+    assert(test_StoI1_EmptyString());
+    assert(test_StoI2_0());
+    assert(test_StoI3_123());
+    assert(test_StoI4_MaxInt());
+    assert(test_StoI5_MaxIntPlus1());
+    assert(test_StoI6_ExtremelyLargeNumber());
+    assert(test_StoI7_VeryLargeNumber());
+    assert(test_StoI8_NonNumeric());
+    assert(test_StoI9_NegativeNumber());
+    assert(test_StoI10_NegativeMaxInt());
+    assert(test_StoI11_PositiveMaxInt());
+    assert(test_StoI12_NegativeMaxIntMinus1());
+    assert(test_StoI14_NegativeZero());
+    assert(test_StoI15_PositiveZero());
+    assert(test_StoI16_VeryLargeNegativeNumber());
 }
