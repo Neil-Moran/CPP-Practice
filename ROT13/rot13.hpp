49dcebc57cefb64fc72ec33a700749204c887a92
Neil-Moran <neilmoran007@gmail.com>
2025-03-16 13:48:19 +0000

    Refactor into findRot13Pairs function, add comments

--- ROT13/rot13.hpp
+++ ROT13/rot13.hpp
@@ -1,25 +1,93 @@
 #pragma once
+
+#include <algorithm>
+#include <fstream>
+#include <stdio.h>
 #include <string>
+#include <vector>
+#include "timing.h"
 
 using std::string;
 
+// return the ROT13 of the input word
 string rot13(string w)
 {
-    string result = w;
-
-    for(int i=0; result[i]; ++i)
+    for(int i=0; w[i]; ++i)
     {
-        if(result[i] >= 'a' && result[i] <= 'm'
-            || result[i] >= 'A' && result[i] <= 'M')
+        // if char is A-M, shift it 13 forward
+        if(w[i] >= 'a' && w[i] <= 'm'
+            || w[i] >= 'A' && w[i] <= 'M')
         {
-            result[i] += 13;
+            w[i] += 13;
         }
-        else if(result[i] >= 'n' && result[i] <= 'z'
-            || result[i] >= 'N' && result[i] <= 'Z')
+        // if char is A-N, shift it 13 backward
+        else if(w[i] >= 'n' && w[i] <= 'z'
+            || w[i] >= 'N' && w[i] <= 'Z')
         {
-            result[i] -=13;
+            w[i] -=13;
         }
     }
 
-    return result;
+    return w;
+}
+
+string findRot13Pairs(string file, size_t minLength)
+{
+    std::vector<string> wordList;
+    
+    // parse dictionary into list
+    {
+        Timer t1;
+        std::ifstream fs;
+        fs.open(file);
+        string word;
+
+        while(std::getline(fs, word))
+        {
+            // ignore words that are shorter that minLength
+            if(word.size() < minLength)
+            {
+                continue;
+            }
+
+            // if word is in first half of alphabet, ROT13 it and add to list
+            if(word[0] <= 'm' || word[0] <= 'M')
+            {
+                wordList.push_back(rot13(word));
+            }
+            // if word is in second half of alphabet, just add it to list
+            else
+            {
+                wordList.push_back(word);            
+            }
+        }
+
+        fs.close();
+        printf("Parsed dictionary\n");
+    } //destroy t1 and print time
+
+    // sort list
+    {
+        Timer t2;        
+        std::sort(wordList.begin(), wordList.end());
+        printf("\nSorted list\n");
+    } //destroy t2 and print time
+
+    string output;
+
+    // find duplicates in list, these are are ROT13 pairs
+    {
+        Timer t3;
+        for(size_t i = 0; i < wordList.size() - 1; ++i)
+        {
+            if(wordList[i] == wordList[i+1])
+            {
+                output += rot13(wordList[i]) + "\t<->\t" + wordList[i] + "\n";
+                ++i; // skip the duplicate word
+            }
+        }
+        printf("\nFound duplicates in list\n");
+    } //destroy t3 and print time    
+
+    return output;
 }
